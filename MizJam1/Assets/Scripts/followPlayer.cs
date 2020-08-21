94c17e36f02080502f024e386ae0348555b23535
Wendell <wendellfranco99@gmail.com>
2020-08-21 12:35:21 -0300

    Follow Camera Smooth Code
    Simple as that

--- MizJam1/Assets/Scripts/followPlayer.cs
+++ MizJam1/Assets/Scripts/followPlayer.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class followPlayer : MonoBehaviour
 {
     public Transform player;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
 
     private void Start()
     {
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (player)
         {
-            transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+            Vector3 desiredPosition = player.transform.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+
+            //transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
         }
     }
 }
